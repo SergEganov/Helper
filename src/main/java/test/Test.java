6de426e4eb763921f2298708445090f72f9577f6
Serega <shinigami358@gmail.com>
2019-12-26 13:08:33 +0300

    test

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Test {
+    private final String x = "ha";
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        System.out.println(arr.length);
+        String s = "test";
+        boolean b;
+    }
+    class Test2 {
+        int x = 0;
+        float f = 0;
+
+    }
+}
